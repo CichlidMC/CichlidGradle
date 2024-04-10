446cd27cfdbaaebb357c9ea031f10ef318356731
TropheusJ <jverrellijr@icloud.com>
2024-04-10 00:59:19 -0400

    set up extension

--- src/main/java/io/github/tropheusj/cichlid_gradle/CichlidGradlePlugin.java
+++ src/main/java/io/github/tropheusj/cichlid_gradle/CichlidGradlePlugin.java
@@ -1,14 +1,11 @@
 package io.github.tropheusj.cichlid_gradle;
 
-import io.github.tropheusj.cichlid_gradle.pistonmeta.VersionManifest;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class CichlidGradlePlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
-		VersionManifest fetch = VersionManifest.fetch();
-		System.out.println("manifest: " + fetch);
-//		project.getRepositories().add()
+		CichlidExtension extension = project.getExtensions().create("cichlid", CichlidExtension.class);
 	}
 }
