18d88ecc31a6827519da78b94582677bbd8405fb
TropheusJ <jverrellijr@icloud.com>
2024-08-16 14:39:16 -0400

    add beforeApply

--- plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/extension/CichlidExtension.java
+++ plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/extension/CichlidExtension.java
@@ -1,11 +1,19 @@
 package io.github.cichlidmc.cichlid_gradle.extension;
 
 import io.github.cichlidmc.cichlid_gradle.run.RunConfiguration;
+import org.gradle.api.Action;
 import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.Project;
 
 public interface CichlidExtension {
 	NamedDomainObjectContainer<RunConfiguration> getRuns();
+
+	/**
+	 * Actions registered here are invoked right before Cichlid applies the Minecraft maven and dependency.
+	 * It is safe to register additional actions in callbacks, matching the behavior of {@link Project#afterEvaluate(Action)}.
+	 * This may be useful in niche situations.
+	 */
+	void beforeApply(Action<? super Project> action);
 
 	static void setup(Project project) {
 		project.getExtensions().create(CichlidExtension.class, "cichlid", CichlidExtensionImpl.class);
--- plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/extension/CichlidExtensionImpl.java
+++ plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/extension/CichlidExtensionImpl.java
@@ -1,19 +1,35 @@
 package io.github.cichlidmc.cichlid_gradle.extension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import io.github.cichlidmc.cichlid_gradle.cache.CichlidCache;
 import io.github.cichlidmc.cichlid_gradle.extension.minecraft.MinecraftExtension;
 import io.github.cichlidmc.cichlid_gradle.run.RunTaskGeneration;
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 
 public abstract class CichlidExtensionImpl implements CichlidExtension {
+	private final List<Action<? super Project>> beforeApplyActions = new ArrayList<>();
+
 	@Inject
 	public CichlidExtensionImpl(Project project) {
 		project.afterEvaluate(this::apply);
 	}
 
+	@Override
+	public void beforeApply(Action<? super Project> action) {
+		this.beforeApplyActions.add(action);
+	}
+
 	private void apply(Project project) {
+		//noinspection ForLoopReplaceableByForEach - used so adding more to the end while iterating is safe
+		for (int i = 0; i < this.beforeApplyActions.size(); i++) {
+			this.beforeApplyActions.get(i).execute(project);
+		}
+
 		MinecraftExtension mc = MinecraftExtension.get(project);
 		String mcVer = mc.getVersion();
 		CichlidCache cache = CichlidCache.get(project);
