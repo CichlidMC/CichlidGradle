91d7b68faf4a700fd7dfc2386742c28b38553d6b
TropheusJ <jverrellijr@icloud.com>
2024-11-12 17:43:20 -0500

    re-enable natives extraction

--- plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/cache/NativesStorage.java
+++ plugin/src/main/java/io/github/cichlidmc/cichlid_gradle/cache/NativesStorage.java
@@ -6,12 +6,14 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import io.github.cichlidmc.cichlid_gradle.util.FileUtils;
 import io.github.cichlidmc.pistonmetaparser.FullVersion;
-import io.github.cichlidmc.pistonmetaparser.version.library.Artifact;
+import io.github.cichlidmc.pistonmetaparser.version.library.Classifier;
 import io.github.cichlidmc.pistonmetaparser.version.library.Library;
+import io.github.cichlidmc.pistonmetaparser.version.library.Natives;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 
@@ -30,24 +32,26 @@ public class NativesStorage {
 		Path dir = this.root.resolve(version.id);
 
 		for (Library library : version.libraries) {
-
-//			library.natives.flatMap(Natives::choose).ifPresent(artifact -> {
-//				logger.quiet("Extracting natives for library {}", library.name);
-//				this.downloadAndExtract(artifact, dir);
-//			});
+			Optional<Classifier> maybeClassifier = library.natives.flatMap(Natives::choose);
+			if (maybeClassifier.isPresent()) {
+				Classifier classifier = maybeClassifier.get();
+				logger.quiet("Extracting natives for library {}: classifier {}", library.name, classifier.name);
+				this.downloadAndExtract(classifier, dir);
+			}
 		}
 	}
 
-	private void downloadAndExtract(Artifact artifact, Path dir) throws IOException {
+	private void downloadAndExtract(Classifier classifier, Path dir) throws IOException {
 		Path tempJar = dir.resolve(TEMP_JAR);
-		FileUtils.downloadSilently(artifact, tempJar);
+		FileUtils.downloadSilently(classifier.artifact, tempJar);
 
 		try (FileSystem fs = FileSystems.newFileSystem(tempJar)) {
 			// jar should have 1 root
 			Path root = fs.getRootDirectories().iterator().next();
 			try (Stream<Path> stream = Files.list(root)) {
 				// the version manifest specifies files that should be excluded from extraction,
-				// but it's undocumented and a pain to handle, and it doesn't even matter in the end.
+				// but it's undocumented and a pain to handle, and it doesn't even matter in the end
+				// as far as I know.
 				for (Iterator<Path> itr = stream.iterator(); itr.hasNext();) {
 					Path file = itr.next();
 					Path dest = dir.resolve(file.getFileName().toString());
